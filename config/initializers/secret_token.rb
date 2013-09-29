6294c476798b49da9ce743318ee98c118df3b75b
taichi-n <taichi@mbp13.local>
2013-09-30 06:58:47 +0900

    very first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwClone::Application.config.secret_token = 'b5e73282190125d80a3373a34986a15df2902218890bab2613e3ed65e5a3c33e397ca40aa503cc3947754cf2150120747e159fcd78d3049eac70626ecdc9ec91'
